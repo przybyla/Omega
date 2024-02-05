57ecf76da56970c1563aa2d1fdf5eb90cb4be5aa
Michal <michal.przybyla.mp@gmail.com>
2024-02-05 20:28:20 +0100

    Handle click and drop on inventory

--- Scripts/InventoryData.gd
+++ Scripts/InventoryData.gd
@@ -1,4 +1,28 @@
 extends Resource
 class_name InventoryData
 
+signal inventory_updated(inventory_data: InventoryData)
+signal inventory_interact(inventory_data: InventoryData, index: int, button: int)
+
 @export var slot_datas: Array[SlotData]
+
+func grab_slot_data(index: int) -> SlotData:
+	var slot_data = slot_datas[index]
+	
+	if slot_data:
+		slot_datas[index] = null
+		inventory_updated.emit(self)
+		return slot_data
+	else:
+		return null
+		
+func drop_slot_data(grabbed_slot_data: SlotData, index: int) -> SlotData:
+	var slot_data = slot_datas[index]
+	
+	slot_datas[index] = grabbed_slot_data
+	inventory_updated.emit(self)
+	return slot_data
+
+
+func on_slot_clicked(index: int, button: int) -> void:
+	inventory_interact.emit(self, index, button)
