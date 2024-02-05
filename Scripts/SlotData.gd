19d347f328100e32eea409adf81469d94a090475
Michal <michal.przybyla.mp@gmail.com>
2024-02-05 20:40:03 +0100

    Add merge items on drop

--- Scripts/SlotData.gd
+++ Scripts/SlotData.gd
@@ -6,8 +6,16 @@ const MAX_STACK_SIZE: int = 99
 @export var item_data: ItemData
 @export_range(1, MAX_STACK_SIZE) var quantity: int = 1: set = set_quantity
 
+func can_fully_merge_with(other_slot_data: SlotData) -> bool:
+	return item_data == other_slot_data.item_data \
+		and item_data.stackable \
+		and quantity + other_slot_data.quantity < MAX_STACK_SIZE
+	
+func fully_merge_with(other_slot_data: SlotData) -> void:
+	quantity += other_slot_data.quantity
+
 func set_quantity(val: int) -> void:
 	quantity = val
 	if quantity > 1 and not item_data.stackable:
 		quantity = 1
-	push_error("%s is not stackable" % item_data.name)
+	
