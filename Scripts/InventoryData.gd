1920c68a2374cb3dd789f4d292e46c4b2450b70e
Michal <michal.przybyla.mp@gmail.com>
2024-02-05 20:49:01 +0100

    Add drop single slot

--- Scripts/InventoryData.gd
+++ Scripts/InventoryData.gd
@@ -29,7 +29,21 @@ func drop_slot_data(grabbed_slot_data: SlotData, index: int) -> SlotData:
 	inventory_updated.emit(self)
 	return return_slot_data
 
-
+func drop_single_slot_data(grabbed_slot_data: SlotData, index: int) -> SlotData:
+	var slot_data = slot_datas[index]
+	
+	if not slot_data:
+		slot_datas[index] = grabbed_slot_data.create_single_slot_data()
+	elif slot_data.can_merge_with(grabbed_slot_data):
+		slot_data.fully_merge_with(grabbed_slot_data.create_single_slot_data())
+		
+	inventory_updated.emit(self)
+	
+	if grabbed_slot_data.quantity > 0:
+		return grabbed_slot_data
+	else:
+		return null
+		
 
 func on_slot_clicked(index: int, button: int) -> void:
 	inventory_interact.emit(self, index, button)
