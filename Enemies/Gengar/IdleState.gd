21f058fbf2e520a176f8d5324288e05d242622a8
tallyon <skowronski6@gmail.com>
2024-02-10 17:43:09 +0100

    wandering in idle state will go back to starting position if wandered too far

--- Enemies/Gengar/IdleState.gd
+++ Enemies/Gengar/IdleState.gd
@@ -4,9 +4,11 @@ class_name IdleState
 @export var enemy := CharacterBody2D
 @export var wander_time_min: float = 1
 @export var wander_time_max: float = 5
+@export var max_wander_distance_from_start: float = 50
 
 var move_direction : Vector2
 var wander_time : float
+var starting_position: Vector2
 
 func randomize_wander():
 	# we need to generate random duration of this wander
@@ -16,8 +18,15 @@ func randomize_wander():
 	# value 0 will be casted to bool as FALSE, any other value (>0) will be casted to bool as TRUE
 	var should_move = randi_range(0, 4) as bool
 	move_direction = Vector2(randf_range(-1, 1), randf_range(1,1)).normalized() if should_move else Vector2.ZERO
+	
+	# if we are going to move, but the max wander distance from starting position has been exceeded
+	# then we should move towards the starting position
+	var current_position_to_starting_position = starting_position - enemy.position
+	if current_position_to_starting_position.length() > max_wander_distance_from_start:
+		move_direction = current_position_to_starting_position.normalized()
 
 func Enter():
+	starting_position = enemy.position
 	randomize_wander()
 
 func state_update(delta: float):
