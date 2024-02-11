a8e439a3278a99e4dadafe0d5b88b14e37659736
tallyon <skowronski6@gmail.com>
2024-02-11 20:02:49 +0100

    refactor

--- Scenes/InventoryInterface.gd
+++ Scenes/InventoryInterface.gd
@@ -15,19 +15,17 @@ func set_player_inventory_data(inventory_data: InventoryData) -> void:
 
 
 func on_inventory_interact(inventory_data: InventoryData, index: int, button: int) -> void:
-
-		match [grabbed_slot_data, button]:
-			[null, MOUSE_BUTTON_LEFT]:
-				grabbed_slot_data = inventory_data.grab_slot_data(index)
-			[_, MOUSE_BUTTON_LEFT]:
-				grabbed_slot_data = inventory_data.drop_slot_data(grabbed_slot_data, index)
-			[null, MOUSE_BUTTON_RIGHT]:
-				pass
-			[_, MOUSE_BUTTON_RIGHT]:
-				grabbed_slot_data = inventory_data.drop_single_slot_data(grabbed_slot_data, index)
-				
-				
-		update_grabbed_slot()
+	match [grabbed_slot_data, button]:
+		[null, MOUSE_BUTTON_LEFT]:
+			grabbed_slot_data = inventory_data.grab_slot_data(index)
+		[_, MOUSE_BUTTON_LEFT]:
+			grabbed_slot_data = inventory_data.drop_slot_data(grabbed_slot_data, index)
+		[null, MOUSE_BUTTON_RIGHT]:
+			pass
+		[_, MOUSE_BUTTON_RIGHT]:
+			grabbed_slot_data = inventory_data.drop_single_slot_data(grabbed_slot_data, index)
+			
+	update_grabbed_slot()
 
 func update_grabbed_slot() -> void:
 	if grabbed_slot_data:
