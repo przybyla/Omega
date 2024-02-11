8ac47a6e77a499117835d8192482cfbddca53984
tallyon <skowronski6@gmail.com>
2024-02-11 16:50:12 +0100

    idle state now has exported var standing in place chance

--- Enemies/Gengar/IdleState.gd
+++ Enemies/Gengar/IdleState.gd
@@ -4,6 +4,8 @@ class_name IdleState
 @export var wander_time_min: float = 1
 @export var wander_time_max: float = 5
 @export var max_wander_distance_from_start: float = 50
+# values range 0.0 (0%) to 1.0 (100%)
+@export var probability_of_standing_in_place: float = 0.25
 
 var player : CharacterBody2D
 var move_direction : Vector2
@@ -17,7 +19,7 @@ func randomize_wander():
 	
 	# 25% (1 in 4) chance that the move direction will be zero to enable enemy to idle in place
 	# value 0 will be casted to bool as FALSE, any other value (>0) will be casted to bool as TRUE
-	var should_move = randi_range(0, 4) as bool
+	var should_move = randi_range(0, (int)(1.0 / probability_of_standing_in_place)) as bool
 	move_direction = Vector2(randf_range(-1, 1), randf_range(1,1)).normalized() if should_move else Vector2.ZERO
 	
 	# if we are going to move, but the max wander distance from starting position has been exceeded
